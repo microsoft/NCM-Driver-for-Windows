27d2aac25673fcf2567e88ac3c47fff93eb41bea
Brandon Jiang <jiangyue@microsoft.com>
2019-04-18 14:15:00 -0700

    fix whitespace

--- inc/NetPacketLibrary.h
+++ inc/NetPacketLibrary.h
@@ -10,102 +10,102 @@
 inline
 SIZE_T
 GetTxPacketDataLength(
-	_In_ NET_RING_PACKET_ITERATOR const* Iterator
+    _In_ NET_RING_PACKET_ITERATOR const* Iterator
 )
 {
-	SIZE_T length = 0;
+    SIZE_T length = 0;
 
-	for (NET_RING_FRAGMENT_ITERATOR fi = NetPacketIteratorGetFragments(Iterator);
-		NetFragmentIteratorHasAny(&fi);
-		NetFragmentIteratorAdvance(&fi))
-	{
-		NET_FRAGMENT* fragment = NetFragmentIteratorGetFragment(&fi);
-		length += (SIZE_T)fragment->ValidLength;
-	}
+    for (NET_RING_FRAGMENT_ITERATOR fi = NetPacketIteratorGetFragments(Iterator);
+        NetFragmentIteratorHasAny(&fi);
+        NetFragmentIteratorAdvance(&fi))
+    {
+        NET_FRAGMENT* fragment = NetFragmentIteratorGetFragment(&fi);
+        length += (SIZE_T)fragment->ValidLength;
+    }
 
-	return length;
+    return length;
 }
 
 inline
 VOID
 CompleteTxPacketsBatch(
-	_In_ NET_RING_COLLECTION const* Rings,
-	_In_ UINT32 BatchSize
+    _In_ NET_RING_COLLECTION const* Rings,
+    _In_ UINT32 BatchSize
 )
 {
-	UINT32 packetCount = 0;
+    UINT32 packetCount = 0;
 
-	NET_RING_PACKET_ITERATOR pi = NetRingGetDrainPackets(Rings);
+    NET_RING_PACKET_ITERATOR pi = NetRingGetDrainPackets(Rings);
 
-	while (NetPacketIteratorHasAny(&pi))
-	{
-		NET_PACKET* packet = NetPacketIteratorGetPacket(&pi);
+    while (NetPacketIteratorHasAny(&pi))
+    {
+        NET_PACKET* packet = NetPacketIteratorGetPacket(&pi);
 
-		// this function uses Scratch field as the bit for testing completion
-		if (!packet->Scratch)
-		{
-			break;
-		}
+        // this function uses Scratch field as the bit for testing completion
+        if (!packet->Scratch)
+        {
+            break;
+        }
 
-		packetCount++;
+        packetCount++;
 
-		NET_RING_FRAGMENT_ITERATOR fi = NetPacketIteratorGetFragments(&pi);
-		NetFragmentIteratorAdvanceToTheEnd(&fi);
+        NET_RING_FRAGMENT_ITERATOR fi = NetPacketIteratorGetFragments(&pi);
+        NetFragmentIteratorAdvanceToTheEnd(&fi);
 
-		NetPacketIteratorAdvance(&pi);
+        NetPacketIteratorAdvance(&pi);
 
-		if (packetCount >= BatchSize)
-		{
-			NetPacketIteratorSet(&pi);
-			Rings->Rings[NET_RING_TYPE_FRAGMENT]->BeginIndex = NetFragmentIteratorGetIndex(&fi);
-		}
-	}
+        if (packetCount >= BatchSize)
+        {
+            NetPacketIteratorSet(&pi);
+            Rings->Rings[NET_RING_TYPE_FRAGMENT]->BeginIndex = NetFragmentIteratorGetIndex(&fi);
+        }
+    }
 }
 
 inline
 SIZE_T
 CopyTxPacketDataToBuffer(
-	_Out_writes_bytes_(BufferLength) PUCHAR BufferDest,
-	_In_ NET_RING_PACKET_ITERATOR const* Iterator,
-	_In_ SIZE_T BufferLength)
+    _Out_writes_bytes_(BufferLength) PUCHAR BufferDest,
+    _In_ NET_RING_PACKET_ITERATOR const* Iterator,
+    _In_ SIZE_T BufferLength)
 {
-	SIZE_T bytesCopied = 0;
+    SIZE_T bytesCopied = 0;
 
-	for (NET_RING_FRAGMENT_ITERATOR fi = NetPacketIteratorGetFragments(Iterator);
-		NetFragmentIteratorHasAny(&fi) && (BufferLength > 0);
-		NetFragmentIteratorAdvance(&fi))
-	{
-		NET_FRAGMENT* fragment = NetFragmentIteratorGetFragment(&fi);
+    for (NET_RING_FRAGMENT_ITERATOR fi = NetPacketIteratorGetFragments(Iterator);
+        NetFragmentIteratorHasAny(&fi) && (BufferLength > 0);
+        NetFragmentIteratorAdvance(&fi))
+    {
+        NET_FRAGMENT* fragment = NetFragmentIteratorGetFragment(&fi);
 
-		UCHAR const* pPacketData = (UCHAR const*)fragment->VirtualAddress + fragment->Offset;
-		SIZE_T bytesToCopy =
-			(BufferLength < (SIZE_T)fragment->ValidLength) ? BufferLength : (SIZE_T)fragment->ValidLength;
-		RtlCopyMemory(BufferDest, pPacketData, bytesToCopy);
+        UCHAR const* pPacketData = (UCHAR const*)fragment->VirtualAddress + fragment->Offset;
+        SIZE_T bytesToCopy =
+            (BufferLength < (SIZE_T)fragment->ValidLength) ? BufferLength : (SIZE_T)fragment->ValidLength;
+        RtlCopyMemory(BufferDest, pPacketData, bytesToCopy);
 
-		bytesCopied += bytesToCopy;
-		BufferDest += bytesToCopy;
-		BufferLength -= bytesToCopy;
-	}
+        bytesCopied += bytesToCopy;
+        BufferDest += bytesToCopy;
+        BufferLength -= bytesToCopy;
+    }
 
-	return bytesCopied;
+    return bytesCopied;
 }
 
 inline
 void
 CancelRxPackets(
-	_In_ NET_RING_COLLECTION const* Rings
+    _In_ NET_RING_COLLECTION const* Rings
 )
 {
-	NET_RING_PACKET_ITERATOR pi = NetRingGetAllPackets(Rings);
+    NET_RING_PACKET_ITERATOR pi = NetRingGetAllPackets(Rings);
 
-	for (; NetPacketIteratorHasAny(&pi); NetPacketIteratorAdvance(&pi))
-	{
-		NetPacketIteratorGetPacket(&pi)->Ignore = 1;
-	}
+    for (; NetPacketIteratorHasAny(&pi); NetPacketIteratorAdvance(&pi))
+    {
+        NetPacketIteratorGetPacket(&pi)->Ignore = 1;
+    }
 
-	NetPacketIteratorSet(&pi);
+    NetPacketIteratorSet(&pi);
 
-	NET_RING_FRAGMENT_ITERATOR fi = NetRingGetAllFragments(Rings);
-	NetFragmentIteratorAdvanceToTheEnd(&fi);
-	NetFragmentIteratorSet(&fi);
+    NET_RING_FRAGMENT_ITERATOR fi = NetRingGetAllFragments(Rings);
+    NetFragmentIteratorAdvanceToTheEnd(&fi);
+    NetFragmentIteratorSet(&fi);
 }
